64743893fdefd62192d33f2d70987ba8d3d52ae7
Juan Castro <jcastro2433@gmail.com>
2023-01-20 18:47:16 -0500

    Clase 6: Hice el aside y la lista de categorías

--- src/main.js
+++ src/main.js
@@ -24,3 +24,50 @@ async function getTrendingPreview() {
     section.appendChild(div);
 }
 getTrendingPreview();
+
+async function getCategories() {
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+    console.log(categories);
+
+    const categoriesdiv = document.querySelector('.categories');
+
+    categories.forEach(category => {
+        const h4 = document.createElement('h4');
+        h4.innerHTML = category.name;
+        categoriesdiv.appendChild(h4);
+    })
+}
+getCategories();
+
+const SpanAside = document.querySelector('.sidebar-div span');
+const SideBar = document.querySelector('.sidebar');
+const Menu = document.querySelector('.menu');
+const categories = document.querySelector('.categories');
+const CategoriesDeploy = document.querySelector('.categories-deploy');
+
+Menu.addEventListener('click', toggleSideBar);
+SpanAside.addEventListener('click', toggleSideBar);
+CategoriesDeploy.addEventListener('click', toggleCategories);
+
+function toggleSideBar() {
+    const isasideclosed = SideBar.classList.contains('inactive');
+
+    if (!isasideclosed) {
+        SideBar.classList.add('inactive');
+    } else{
+        SideBar.classList.remove('inactive');
+    }
+};
+
+function toggleCategories(){
+    const iscategoriesclosed = categories.classList.contains('inactive');
+
+    if (!iscategoriesclosed) {
+        categories.classList.add('inactive');
+    } else{
+        categories.classList.remove('inactive');
+    }
+}
